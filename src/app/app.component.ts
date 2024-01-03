d6b8a423808e7b6e3c4f632495c6f6e93848b3ae
Vishal Kundan <vishalkannak@gmail.com>
2024-01-03 22:14:25 +0530

    joined a component with the main on

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MycompComponent } from './mycomp/mycomp.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  
+  imports: [CommonModule, RouterOutlet,FormsModule,MycompComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Vishal Kanna';
+  name = '';
 }
---
+++ src/app/mycomp/mycomp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mycomp',
+ standalone: true,
+ imports: [],
+  templateUrl: './mycomp.component.html',
+  styleUrl: './mycomp.component.css'
+})
+export class MycompComponent {
+
+}
