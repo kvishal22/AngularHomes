3e2b7655587fd358c39db6e6c8513b341ce20d0f
Vishal Kundan <vishalkannak@gmail.com>
2024-01-04 12:05:55 +0530

    implemented booking

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
+import { HousingLocation } from '../housing-location';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [RouterModule,CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-
+    @Input() housingLocation!:HousingLocation;
 }
